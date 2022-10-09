34bec3ba7d087ce967d82b5f57aa31822d6c7b56
TanPuc <htanphuc1212@gmail.com>
2022-10-09 08:38:08 +0700

    Moving files from old folder to the new one

---
+++ App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import MainContainer from './navigation/MainContainer';
+
+function App() {
+  return (
+    <view>
+      <text>
+        Hello
+      </text>
+    </view>
+  );
+}
+
+export default App();
