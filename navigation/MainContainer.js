ca2d00746c478a84c9c1cd5e7c90bb73089cbf3b
TanPuc <htanphuc1212@gmail.com>
2022-10-10 00:59:26 +0700

    10/10/2022 0:30 a.m.

---
+++ navigation/MainContainer.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Screens
+import HomeScreen from './screens/HomeScreen.js';
+import MapScreen from './screens/MapScreen.js';
+
+//Screen names
+const homeName = "Home";
+const mapName = "Map";
+
+const Tab = createBottomTabNavigator();
+
+function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? 'home' : 'home-outline';
+
+            } else if (rn === mapName) {
+              iconName = focused ? 'map' : 'map-outline';
+
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'grey',
+          labelStyle: { paddingBottom: 10, fontSize: 10 },
+          style: { padding: 10, height: 70}
+        }}>
+
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={mapName} component={MapScreen} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainContainer;
