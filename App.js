308f69aba208403e063ac9853088a77bcf19f13d
Le Quang Phuc <phuclequang2006@gmail.com>
2022-10-14 22:10:06 +0700

    Create SignIn

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
 import { StatusBar } from 'react-native';
+
 import Welcome from './components/Welcome.js';
 import GetStarted from './components/GetStarted.js';
 import SignUp from './components/SignUp.js';
+import SignIn from './components/SignIn.js';
 
 const Stack = createNativeStackNavigator();
 
@@ -43,6 +45,10 @@ export default function App() {
           <Stack.Screen
             name = "SignUp"
             component = {SignUp}
+          />
+          <Stack.Screen
+            name = "SignIn"
+            component = {SignIn}
           />
         </Stack.Navigator>
     </NavigationContainer>
