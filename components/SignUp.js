83e346b3b246a63a2666e3e7bf1c165d9542b294
Le Quang Phuc <phuclequang2006@gmail.com>
2022-10-14 22:09:53 +0700

    Fix and update validation

--- components/SignUp.js
+++ components/SignUp.js
@@ -8,83 +8,133 @@ export default function SignUp({ navigation }) {
   const [inputs, setInputs] = React.useState({
     fullname: '',
     email: '',
-    password: ''
+    password: '',
   });
+  const [errors, setErrors] = React.useState({})
   const validate = () => {
     Keyboard.dismiss();
+    let vaild = true;
+    if(!inputs.email) {
+      handleError('Vui lòng nhập email', 'email');
+      vaild = false;
+    } else if(!inputs.email.match(/\S+@\S+\.\S+/)) {
+      handleError('Định dạng email không chính xác', 'email');
+    }
+
+    if(!inputs.fullname) {
+      handleError('Vui lòng nhập họ tên', 'fullname');
+    }
+
+    if(!inputs.password) {
+      handleError('Vui lòng nhập mật khẩu', 'password');
+    } else if(inputs.password.length < 5) {
+      handleError('Mật khẩu phải có tối thiểu 6 kí tự', 'password');
+    }
   };
+
+  const handleOnChange = (text, input) => {
+    setInputs((prevState) => ({...prevState, [input]: text}));
+  };
+
+  const handleError = (errorMessage, input) => {
+    setErrors((prevState) => ({...prevState, [input]: errorMessage}));
+  };
+
   return (
     <SafeAreaView>
       <ScrollView contentContainerStyle={styles.container}>
-        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
-          <View style={styles.heading}>
-            <Text style={styles.title}>Đăng ký</Text>
-            <Text style={styles.subtitle}>Chúng tôi cần thông tin về bạn để tạo tài khoản</Text>
-          </View>
+        <View style={styles.heading}>
+          <Text style={styles.title}>Đăng ký</Text>
+          <Text style={styles.subtitle}>Chúng tôi cần thông tin về bạn để tạo tài khoản</Text>
+        </View>
 
-          <View style={styles.inputArea}>
-            <InputArea iconName="account-outline" placeholder="Họ và tên" style={styles.textInput}  />
-            <InputArea iconName="email-outline" placeholder="Địa chỉ Email" style={styles.textInput}  />
-            <InputArea iconName="lock-outline" placeholder="Mật khẩu" style={styles.textInput} password />
-            <TouchableOpacity style={styles.signUpBtn}>
-              <Text style={{
-                fontFamily: 'SourceSansPro_Bold',
-                fontSize: '18',
-                color: 'white',
-              }}>Tạo tài khoản</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={styles.inputArea}>
+          <InputArea 
+            iconName="account-outline" 
+            placeholder="Họ và tên" 
+            style={styles.textInput} 
+            error={errors.fullname}
+            onFocus={() => {
+              handleError(null, 'fullname');
+            }}
+            onChangeText={(text) => handleOnChange(text, 'fullname')}
+          />
+          <InputArea 
+            iconName="email-outline" 
+            placeholder="Địa chỉ Email" 
+            style={styles.textInput} 
+            error={errors.email}
+            onFocus={() => {
+              handleError(null, 'email');
+            }}
+            onChangeText={(text) => handleOnChange(text, 'email')}
+          />
+          <InputArea 
+            iconName="lock-outline" 
+            placeholder="Mật khẩu" 
+            style={styles.textInput} 
+            error={errors.password}
+            onFocus={() => {
+              handleError(null, 'password');
+            }}
+            onChangeText={(text) => handleOnChange(text, 'password')}
+            password
+          />
+          <TouchableOpacity activeOpacity={0.7} style={styles.signUpBtn} onPress={validate}>
+            <Text style={{
+              fontFamily: 'SourceSansPro_Bold',
+              fontSize: 18,
+              color: 'white',
+            }}>Tạo tài khoản</Text>
+          </TouchableOpacity>
+        </View>
 
-          <View style={styles.connect}>
-            <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-              <View style={{flex: 1, height: 1, backgroundColor: 'grey', marginLeft: '10%'}} />
-              <View>
-                <Text style={{width: 150, textAlign: 'center', fontFamily: 'SourceSansPro_Light', fontSize: 17, color: 'grey'}}>Hoặc tiếp tục với</Text>
-              </View>
-              <View style={{flex: 1, height: 1, backgroundColor: 'grey', marginRight: '10%'}} />
-            </View>
-            <TouchableOpacity style={styles.withFacebook}>
-              <Icon name="facebook" style={{
-                fontSize: 37,
-                color: 'white',
-                marginLeft: '2%',
-                justifyContent: 'flex-start',
-                alignItems: 'flex-start',
-                position: 'absolute',
-                left: 15,
-              }}></Icon>
-              <Text style={{
-                color: 'white',
-                fontFamily: 'SourceSansPro_Bold',
-                fontSize: 15,
-                alignSelf: 'center',
-              }}>Tiếp tục với Facebook</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.withGoogle}>
-              <Image source={require('../assets/google.png')} style={{
-                position: 'absolute',
-                left: 15,
-                width: 45,
-                height: 45,
-              }}></Image>
-              <Text style={{
-                color: 'black',
-                fontFamily: 'SourceSansPro_Bold',
-                fontSize: 15,
-                alignSelf: 'center',
-              }}>Tiếp tục với Google</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{marginTop: '5%'}}>
-              <Text style={{
-                fontFamily: 'SourceSansPro_Regular',
-                fontSize: 16,
-              }}>Đã có tài khoản? <Text style={{fontSize: 16, color: '#ff757c', fontFamily: 'SourceSansPro_Bold'}}>Đăng nhập</Text></Text>
-            </TouchableOpacity>
+        <View style={styles.connect}>
+          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+            <View style={{flex: 1, height: 1, backgroundColor: 'grey', marginLeft: '10%'}} />
             <View>
-
+              <Text style={{width: 150, textAlign: 'center', fontFamily: 'SourceSansPro_Light', fontSize: 17, color: 'grey'}}>Hoặc tiếp tục với</Text>
             </View>
+            <View style={{flex: 1, height: 1, backgroundColor: 'grey', marginRight: '10%'}} />
           </View>
-        </KeyboardAvoidingView>
+          <TouchableOpacity style={styles.withFacebook}>
+            <Icon name="facebook" style={{
+              fontSize: 37,
+              color: 'white',
+              marginLeft: '2%',
+              justifyContent: 'flex-start',
+              alignItems: 'flex-start',
+              position: 'absolute',
+              left: 15,
+            }}></Icon>
+            <Text style={{
+              color: 'white',
+              fontFamily: 'SourceSansPro_Bold',
+              fontSize: 15,
+              alignSelf: 'center',
+            }}>Tiếp tục với Facebook</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.withGoogle}>
+            <Image source={require('../assets/google.png')} style={{
+              position: 'absolute',
+              left: 15,
+              width: 45,
+              height: 45,
+            }}></Image>
+            <Text style={{
+              color: 'black',
+              fontFamily: 'SourceSansPro_Bold',
+              fontSize: 15,
+              alignSelf: 'center',
+            }}>Tiếp tục với Google</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginTop: '5%', marginBottom: '10%'}}>
+            <Text style={{
+              fontFamily: 'SourceSansPro_Regular',
+              fontSize: 16,
+            }} onPress={() => navigation.replace('SignIn')}>Đã có tài khoản? <Text style={{fontSize: 16, color: '#ff757c', fontFamily: 'SourceSansPro_Bold'}}>Đăng nhập</Text></Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
