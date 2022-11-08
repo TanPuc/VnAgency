038f6bbdeb9656a4d171a58d740e0f845af46730
Le Quang Phuc <phuclequang2006@gmail.com>
2022-11-08 14:22:58 +0700

    dcmpuc

--- components/Main_Screen/MapScreen.js
+++ components/Main_Screen/MapScreen.js
@@ -7,9 +7,6 @@ import * as Location from "expo-location"
 import Constants from 'expo-constants';
 import { BottomPopup } from '../assets/BottomPopup';
 import MapViewDirections from 'react-native-maps-directions';
-// import mapStyle from "../../assets/mapStyle.json";
-
-
 
 const popupList = [
   {
@@ -42,9 +39,6 @@ const MapScreen = ({ navigation }) => {
     latitude: 0,
     longitude: 0,
   })
-  const ref = React.useRef();
-  const mapRef = React.useRef();
-  
   
   useEffect(() => {
     //Getting user location
@@ -82,7 +76,8 @@ const MapScreen = ({ navigation }) => {
   // }
 
   //Adding direction
-
+  console.log(Origin, Destination);
+  
   if(Region.latitude != null && Region.longitude != null)
   return (  
     <View style={styles.container}>
@@ -95,38 +90,36 @@ const MapScreen = ({ navigation }) => {
         showsBuildings={true}
         loadingEnabled={true}
       >
-        {/* <MapViewDirections
+        <MapViewDirections
           origin={Origin}
           destination={Destination}
-          apiKey="AIzaSyDLrAg2LBoIvdFVMSecuZ7a6aoM7bAFJtI"
+          apikey="AIzaSyDLrAg2LBoIvdFVMSecuZ7a6aoM7bAFJtI"
           strokeWidth={3}
           strokeColor="red"
-        ></MapViewDirections> */}
+        ></MapViewDirections>
       </MapView>
 
-        {/* Search bar */}
-        <SafeAreaView style = {styles.searchContainer}> 
-          <GooglePlacesAutocomplete
-            placeholder='Tìm kiếm'
-            fetchDetails={true}
-            // GooglePlacesDetailsQuery={{rankby:'distance'}}
-            onPress={(data, details = null) => {
-              setDestination({
-                latitude: details.geometry.location.lat,
-                longitude: details.geometry.location.lng,
-              });
-              console.log(Destination.latitude, Destination.longitude); 
-              console.log(Origin, Destination);
+      {/* Search bar */}
+      <SafeAreaView style = {styles.searchContainer}> 
+        <GooglePlacesAutocomplete
+          placeholder='Tìm kiếm'
+          fetchDetails={true}
+          // GooglePlacesDetailsQuery={{rankby:'distance'}}
+          onPress={(data, details = null) => {
+            setDestination({
+              latitude: details.geometry.location.lat,
+              longitude: details.geometry.location.lng,
+            });
+        }}
+          onFail={error => console.log(error)}
+          onNotFound={() => console.log('no results')}
+          query={{
+            key: 'AIzaSyDLrAg2LBoIvdFVMSecuZ7a6aoM7bAFJtI',
+            language: 'en',
+            components: "country:vn",
           }}
-            onFail={error => console.log(error)}
-            onNotFound={() => console.log('no results')}
-            query={{
-              key: 'AIzaSyDLrAg2LBoIvdFVMSecuZ7a6aoM7bAFJtI',
-              language: 'en',
-              components: "country:vn",
-            }}
-          />
-        </SafeAreaView>
+        />
+      </SafeAreaView>
       
       {/* <SafeAreaView style = {styles.PopupBox}>
         <TouchableWithoutFeedback 
