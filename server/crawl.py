d53c71a19805458da85da798f834a8f0b6d38255
Le Quang Phuc <phuclequang2006@gmail.com>
2022-11-20 22:38:36 +0700

    dangcapquocte

---
+++ server/crawl.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+urls = set()
+
+final = []
+
+for i in range(1, 31):
+    url = "https://baodanang.vn/du-lich-da-nang/?paged=" + str(i)
+    
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+    
+    for link in soup.findAll('a'):
+        res = str(link.get('href'))
+        
+        if(res != '/du-lich-da-nang/' and res != '@PAGE_URL@'):
+            urls.add("https://baodanang.vn" + res)
+
+for link in urls:
+    r = requests.get(link)
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    news = {}
+
+    title = soup.title.text.split(" -")[0]
+    abstract = soup.select_one("strong").text
+    image = "https://baodanang.vn" + str(soup.findAll('img')[0]['src'])
+    date = soup.find_all("div", {"class": "date"})[0].text.split(", ")[1]
+
+    if(len(abstract) < 10):
+        continue
+
+    news = {
+        'title': str(title),
+        'abstract': str(abstract),
+        'image': str(image),
+        'date': str(date),
+        'link': str(link),
+    }
+
+    test = requests.get(image)
+    if(test.status_code == 200): 
+        final.append(news)
+
+with open('NEWS.json', 'w', encoding="utf8") as file:
+    json.dump(final, file, indent=4, ensure_ascii=False)
