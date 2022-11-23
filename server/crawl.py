404f723730da88d072f762a117ff0c9083a7251b
Le Quang Phuc <phuclequang2006@gmail.com>
2022-11-23 13:05:51 +0700

    handsomecode

--- server/crawl.py
+++ server/crawl.py
@@ -3,54 +3,115 @@ from bs4 import BeautifulSoup
 import json
 from datetime import datetime
 
-urls = set()
-
-final = []
-
-for i in range(1, 101):
-    url = "https://baodanang.vn/du-lich-da-nang/?paged=" + str(i)
-    
-    r = requests.get(url)
-
-    if(r.status_code != 200):
-        break
-
-    soup = BeautifulSoup(r.content, 'html.parser')
-    
-    for link in soup.findAll('a'):
-        res = str(link.get('href'))
+# Get news
+def NEWS():
+    urls = set()
+    final = []
+    for i in range(1, 101):
+        url = "https://baodanang.vn/du-lich-da-nang/?paged=" + str(i)
         
-        if(res != '/du-lich-da-nang/' and res != '@PAGE_URL@'):
-            urls.add("https://baodanang.vn" + res)
+        r = requests.get(url)
 
-for link in urls:
-    r = requests.get(link)
+        if(r.status_code != 200):
+            break
 
-    soup = BeautifulSoup(r.content, 'html.parser')
+        soup = BeautifulSoup(r.content, 'html.parser')
+        
+        for link in soup.findAll('a'):
+            res = str(link.get('href'))
+            
+            if(res != '/du-lich-da-nang/' and res != '@PAGE_URL@'):
+                urls.add("https://baodanang.vn" + res)
 
-    title = soup.title.text.split(" -")[0]
-    abstract = soup.select_one("strong").text
-    image = "https://baodanang.vn" + str(soup.findAll('img')[0]['src'])
-    date = soup.find_all("div", {"class": "date"})[0].text.split(", ")[1]
+    for link in urls:
+        r = requests.get(link)
 
-    if(len(abstract) < 10):
-        continue
+        soup = BeautifulSoup(r.content, 'html.parser')
 
-    news = {
-        'title': str(title),
-        'abstract': str(abstract),
-        'image': str(image),
-        'date': str(date),
-        'link': str(link),
-    }
+        title = soup.title.text.split(" -")[0]
+        abstract = soup.select_one("strong").text
+        image = "https://baodanang.vn" + str(soup.findAll('img')[0]['src'])
+        date = soup.find_all("div", {"class": "date"})[0].text.split(", ")[1]
 
-    test = requests.get(image)
-    if(test.status_code == 200):
-        final.append(news)
+        if(len(abstract) < 10):
+            continue
 
-final.sort(key = lambda x: datetime.strptime(x['date'], '%d/%m/%Y'))
+        news = {
+            'title': str(title),
+            'abstract': str(abstract),
+            'image': str(image),
+            'date': str(date),
+            'link': str(link),
+        }
 
-final.reverse()
+        test = requests.get(image)
+        if(test.status_code == 200):
+            final.append(news)
 
-with open('NEWS.json', 'w', encoding="utf8") as file:
-    json.dump(final, file, indent=2, ensure_ascii=False)
+    final.sort(key = lambda x: datetime.strptime(x['date'], '%d/%m/%Y'))
+
+    final.reverse()
+
+    with open('NEWS.json', 'w', encoding="utf8") as file:
+        json.dump(final, file, indent=2, ensure_ascii=False)
+
+# Get events
+def EVENTS():
+    urls = set()
+    final = []
+
+    for i in range(1, 4):
+        r = requests.get('https://danangfantasticity.com/event/page/' + str(i));
+
+        soup = BeautifulSoup(r.content, 'html.parser')
+
+        for link in soup.findAll('a'):
+            res = str(link.get('href'))
+            
+            if(res != '@PAGE_URL@' and res.find("/event/") != -1 and res.find("/page/") == -1):
+                urls.add(res)
+
+    for link in urls:
+        r = requests.get(link)
+
+        soup = BeautifulSoup(r.content, 'html.parser')
+
+        title = soup.title.text.split(" -")[0]
+        image = str(soup.findAll('img', attrs={'class':'attachment-post-thumbnail size-post-thumbnail wp-post-image'})[0]['data-src'])
+        start_date = ""
+        end_date = ""
+        location = ""
+
+        for item in soup.find_all("div", {"class": "wrap-date wrap-pro"}):
+            for date in item.find_all("span", {"class": "second_font general-content"}):
+                if(start_date == ""):
+                    start_date = date.text
+                else:
+                    end_date = item.text.split(" - ")[1]
+        
+        for item in soup.find_all("div", {"class": "wrap-location wrap-pro"}):
+            for pos in item.find_all("span", {"class": "second_font general-content"}):
+                location = pos.text
+        
+        event = {
+            'title': str(title),
+            'image': str(image),
+            'start_date': str(start_date),
+            'end_date': str(end_date.replace("\n", "")),
+            'location': str(location.strip()),
+        }
+
+        test = requests.get(image)
+        if(test.status_code == 200):
+            final.append(event)
+    
+    with open('EVENTS.json', 'w', encoding="utf8") as file:
+        json.dump(final, file, indent=2, ensure_ascii=False)
+
+if __name__ == "__main__":
+    print("Crawling NEWS...")
+    NEWS()
+    print("Finished!", end="\n\n")
+    print("Crawling EVENTS...")
+    EVENTS()
+    print("Finished!")
